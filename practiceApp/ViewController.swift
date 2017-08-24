fe7321d16449b9ecab6af7208ded6ba322307a85
Amna <amna@Amnas-MacBook-Pro.local>
2017-08-24 22:06:33 +0400

    first commit

--- practiceApp/ViewController.swift
+++ practiceApp/ViewController.swift
@@ -10,15 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webview: UIWebView!
+    @IBOutlet weak var enter: UIButton!
+    @IBOutlet weak var urlField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+        
+        let url = NSURL(string: urlField.text!)
+        let requestObj = NSURLRequest(URL: url!)
+        webview.loadRequest(requestObj);
+        
+    }
+    
+    
 
 
 }
